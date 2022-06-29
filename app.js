1583f3609e40dfc106ebff3b1c3c45e76a786e88
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-06-29 16:20:20 -0400

    Fix flexbox

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ function createGrid () {
   const mainWrapper = document.querySelector('.mainWrapper')
   const container = document.createElement('div');
   container.classList.add('container');
-  container.style = 'display:flex;';
   mainWrapper.appendChild(container);
   for (let i = 0; i < 4; i++) {
   let row = document.createElement('div');
+  let breaker = document.createElement('div');
   row.classList.add('row');
+  breaker.classList.add('breaker');
   for (let j = 0; j < 4; j++) {
       let block = document.createElement('div');
       block.classList.add('block');
       row.appendChild(block);
   };
   container.appendChild(row);
+  container.appendChild(breaker);
   };
 };
 const gridContainer = document.querySelector('.container');

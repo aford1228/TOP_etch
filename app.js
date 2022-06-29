0ff8900d172747154bc8a6dddb02c3ecf9db4693
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-06-29 14:40:01 -0400

    Replace onload logic with immediate function call

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-document.body.onload = createGrid;
+createGrid();
 
 function createGrid () {
   const container = document.createElement('div');
   container.classList.add('container');
-  body.appendChild(container);
+  container.style = 'display:flex;';
+  document.body.appendChild(container);
   for (let i = 0; i < 4; i++) {
   let row = document.createElement('div');
   row.classList.add('row');
   for (let j = 0; j < 4; j++) {
       let block = document.createElement('div');
-      block.classList.add('grid');
+      block.classList.add('block');
       row.appendChild(block);
   };
   container.appendChild(row);
   };
 };
-
-const body = document.querySelector('body');
 const gridContainer = document.querySelector('.container');
-const grid = document.querySelectorAll('.grid');
+const rows = document.querySelectorAll('.row');
+const blocks = document.querySelectorAll('.block');

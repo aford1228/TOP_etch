668df18ccb8f06445f2ca8db5ba9cff49c651559
aford1228 <105509125+aford1228@users.noreply.github.com>
2022-07-02 15:30:24 -0400

    Begin mode implementation

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
+// primary layout devices
 const mainContent = document.querySelector('#mainContent');
 const container = document.querySelector('#container');
 const slider = document.querySelector('.slider');
-const resetButton = document.querySelector('#etchReset');
-resetButton.addEventListener('click', reset);
+
+// default settings values
+const DEFAULT_SIZE = 50;
+const DEFAULT_MODE = 'monocolor';
+let curSize = DEFAULT_SIZE;
+let curMode = DEFAULT_MODE;
+
+// button actions
 const picker = document.querySelector('#picker');
 picker.oninput = (e) => changeActiveColor(e.target.value);
 
-const DEFAULT_COLOR = '#000000';
+const resetButton = document.querySelector('#etchReset');
+resetButton.addEventListener('click', reset);
+
+const monoButton = document.querySelector('#monocolor');
+monoButton.addEventListener('click', changeMode('monocolor'));
+
+const rainbowButton = document.querySelector('#rainbow');
+rainbowButton.addEventListener('click', changeMode('rainbow'));
+
+const eraserButton = document.querySelector('#eraser');
+eraserButton.addEventListener('click', changeMode('eraser'));
+
 let curColor = picker.value;
-
-const DEFAULT_SIZE = 50;
-let curSize = DEFAULT_SIZE;
-
-//const DEFAULT_MODE = rainbow;
-//let curMode = DEFAULT_MODE;
 
 let mouseDown = false;
 document.body.onmousedown = () => mouseDown = true;
@@ -31,18 +43,48 @@ function createGrid (size) {
   };
 };
 
+function changeMode (newMode) {
+  buttonMode(newMode);
+  curMode = newMode;
+}
+
+function buttonMode(newMode) {
+  if (curMode === 'monocolor') {
+    monoButton.classList.remove('active')
+  }
+  else if (curMode === 'rainbow') {
+    rainbowButton.classList.remove('active')
+  }
+  else if (curMode === 'eraser') {
+    eraserButton.classList.remove('active')
+  }
+
+  if (newMode === 'monocolor') {
+    monoButton.classList.add('active')
+  }
+  else if (newMode === 'rainbow') {
+    rainbowButton.classList.add('active')
+  }
+  else if (newMode === 'eraser') {
+    eraserButton.classList.add('active')
+  };
+}
+
 function colorMorph (e) {
   if (e.type === 'mouseover' && !mouseDown) return;
-  //if (curMode == monocolor) {
-   // e.target.style.backgroundColor = curColor;
-  //}
-  //else if (curMode = rainbow) {
+  if (curMode === 'monocolor') {
+    e.target.style.backgroundColor = curColor;
+  }
+  else if (curMode === 'rainbow') {
     let r = ~~((Math.random()) * 256);
     let g = ~~((Math.random()) * 256);
     let b = ~~((Math.random()) * 256);
     e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
   }
-
+  else if (curMode === 'eraser') {
+    e.target.style.backgroundColor = '#f5f5f5';
+  }
+}
 
 function changeActiveColor (newColor) {
   curColor = newColor;
@@ -55,10 +97,6 @@ function reset () {
   setTimeout(() => container.classList.toggle('shake'), 1000);
   setTimeout(() => container.innerHTML = '', 900);
   setTimeout(() => createGrid(curSize), 1000);
-}
-
-function toggleStatus(e) {
-  e.target.classList.toggle('active');
 }
 
 window.onload = () => {
